2fb8436ab4c928eb1c5863b24a8446877b5d0400
William Ibiapina <williamribeiroibiapina@gmail.com>
2023-08-16 20:14:07 -0300

    Creating components and pages

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  type: string = "password"
+  isText: boolean = false
+  eyeIcon: string = "fa-eye-slash"
 
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  mostrarEsconderSenha() {
+    this.isText = !this.isText
+    this.eyeIcon = this.isText ? "fa-eye" : "fa-eye-slash"
+    this.type = this.isText ? "text" : "password"
+  }
 }
--- Frontend/src/app/components/signup/signup.component.ts
+++ Frontend/src/app/components/signup/signup.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  type: string = "password"
+  isText: boolean = false
+  eyeIcon: string = "fa-eye-slash"
 
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  mostrarEsconderSenha() {
+    this.isText = !this.isText
+    this.eyeIcon = this.isText ? "fa-eye" : "fa-eye-slash"
+    this.type = this.isText ? "text" : "password"
+  }
 }
